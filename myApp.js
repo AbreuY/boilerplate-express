6822a66a1f5967823abad9503ff04a55503b2d35
Abreu Abreu <yolfran12@gmail.com>
2022-02-09 22:46:56 +0000

    serving static content, serving json data

--- myApp.js
+++ myApp.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World")
+//absolute path to index.html
+let absolutePath = __dirname + "/views/index.html";
+//path to public folder
+let pathStatic = __dirname + "/public";
 
+// Serve static content for the app from the 
+// “public” directory in the application directory:
+app.use(pathStatic, express.static(__dirname + pathStatic));
 
+app.get('/', (req, res)=>{
+//res.send("Hello Express");
+res.sendFile(absolutePath);
+});
 
+//Serving json data
+app.get('/json', (req, res)=>{
+  res.json({
+    "message":"Hello json"
+  });
+});
 
 
 
