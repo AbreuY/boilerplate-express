147b209d302189fd3956282170819e63fdae6948
Abreu Abreu <yolfran12@gmail.com>
2022-02-10 02:05:44 +0000

    Added now and name endpoint

--- myApp.js
+++ myApp.js
@@ -9,26 +9,57 @@ let pathStatic = __dirname + "/public";
 
 // Serve static content for the app from the 
 // “public” directory in the application directory:
-app.use(pathStatic, express.static(__dirname + pathStatic));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res)=>{
-//res.send("Hello Express");
-res.sendFile(absolutePath);
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.get('/', (req, res) => {
+  //res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
 
 //Serving json data
-app.get('/json', (req, res)=>{
-  if(process.env.MESSAGE_STYLE == uppercase){
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
     res.json({
-      "message":"HELLO JSON"
+      "message": "HELLO JSON"
     });
 
     return;
   }
   res.json({
-    "message":"Hello json"
+    "message": "Hello json"
   });
   return;
+});
+
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ "time": req.time });
+});
+
+app.get('/:word/echo', (req, res)=>{
+  res.json({
+    "echo": req.params.word
+  });
+});
+
+app.get('/name', (req, res)=>{
+  console.log(req.query);
+  let {first, last} = req.query;
+  res.json({
+    "name":first + " " + last 
+  });
+}).post('/name', (req, res)=>{
+    let {first, last} = req.query;
+  res.json({
+    "name":first + " " + last
+  });
 });
 
 
@@ -60,5 +91,4 @@ app.get('/json', (req, res)=>{
 
 
 
-
- module.exports = app;
+module.exports = app;
